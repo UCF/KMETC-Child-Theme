b76ed1976d1fa974c2a88ba3dbc6b15b66e9f290
Joseph Giron <jgiron@ucf.edu>
2023-02-01 09:30:56 -0500

    adding Knightbot code

--- footer.php
+++ footer.php
@@ -2,5 +2,11 @@
 		</main>
 		<?php echo ucfwp_get_footer_markup(); ?>
 		<?php wp_footer(); ?>
+		<!-- DO NOT EDIT BELOW THIS LINE -->
+		<!-- Mainstay Web Chat -->
+		<script>window.admitHubBot = {botToken: "cad98cc8b39341349", collegeId: "universityCentralFlorida"};</script>
+		<script async src="https://webbot.mainstay.com/static/js/webchat.js"></script>
+		<link rel="stylesheet" type="text/css" href="https://webbot.mainstay.com/static/css/webchat.css">
+		<!-- DO NOT EDIT ABOVE THIS LINE -->
 	</body>
 </html>
